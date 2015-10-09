34a2a1c4e589de249bd0288af8eddb341557addd
zxs1215 <ujjfvswc@gmail.com>
2015-10-09 21:30:23 +0800

    to 29

---
+++ ex27.py
@@ -0,0 +1,39 @@
+# NOT
+print not False
+print not True
+
+# OR
+print True or False
+print True or True
+print False or True
+print False or False
+
+# AND
+print True and False
+print True and True
+print False and True
+print False and False
+
+# NOT OR
+print not (True or False)
+print not (True or True)
+print not (False or True)
+print not (False or False)
+
+# NOT AND
+print not (True and False)
+print not (True and True)
+print not (False and True)
+print not (False and False)
+
+# !=
+print 1 != 0
+print 1 != 1
+print 0 != 1
+print 0 != 0
+
+# ==
+print 1 == 0
+print 1 == 1
+print 0 == 1
+print 0 == 0
